31d2c857c08aabebf1aba9b7e8e2a5f992192a4d
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-07-31 02:05:14 +0000

    hero commit

---
+++ src/components/Cards/DiscoverCards.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import "./discoverCards.css"
+
+const DiscoverCards: React.FC = () => {
+  // Your component logic here
+  return (
+    <><div className="swiperContainer">Swiiiiiiiiiii</div></>    
+  );
+};
+
+export default DiscoverCards;
--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./discover.css"
-import MySwiper from "../Swiper/Swiper";
+import MySwiper from "../Swiper/MySwiper";
 
 const Discover: React.FC = () => {
   // Your component logic here
   return (
    <>
+   <div className="discover">
    <div className="discoverTextArea">
-          <span>Where to go, right now</span>
+          <h1>Where to go, right now</h1>
           <p>Spots at the top of travelers must go list</p>
    </div>
    <MySwiper />
+   </div>
    </> 
   );
 };
