f1f272c3a1b9d32ce5badbfdf3bbe646228a0acb
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-07-31 03:43:18 +0000

    swiper commit

--- src/components/Swiper/MySwiper.tsx
+++ src/components/Swiper/MySwiper.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import "./swiper.css"
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css"; // Import Swiper styles
+import "./swiper.css";
 
 const MySwiper: React.FC = () => {
-  // Your component logic here
   return (
-    <><div className="text-roseColor swiperContainer">
-      Swiiiiiiiiiii
-      </div></>    
+    <div className="text-grey swiperContainer">
+      <Swiper
+       className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
