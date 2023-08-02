66dfc2a5efd954082e9e401d91296bd61df7309f
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-08-02 08:28:02 +0000

    cover content

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import "./cover.css"
+import "./cover.css";
+import coverimage from "../../assets/images/homepage/cover.png"
 
 
 const Cover: React.FC = () => {
   return (
     <>
     <div className='coverframe'>
-     covvvvvvvvvv
+     <div className='covercontent'>
+     <img src={coverimage} alt="cover img" />
+     </div>
     </div>
     </>
   );
