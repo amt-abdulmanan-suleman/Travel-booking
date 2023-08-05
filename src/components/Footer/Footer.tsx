26d24cd2086afebf7782194e15c4d6124a1d5710
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-08-05 14:57:58 +0000

    footer components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./footer.css"
 import Logo from "../Logo/Logo";
+import socialsImg from "../../assets/icons/Group 8.svg"
 
 const Footer: React.FC = () => {
   // Your component logic here
@@ -8,7 +9,26 @@ const Footer: React.FC = () => {
    <>
    <div className="footer">
    <div className="footerTextArea">
+
+    {/*  */}
+     <div className="right">
      <Logo />
+     <p>Get the Travelcity Newsletter<br/>Email address (required)</p>
+     <div className="buttongroup">
+      <input type="text" className="inputIcon" placeholder="Enter your email address" />
+      <button>Subscribe</button>
+     </div>
+     <h5>
+     This site is protected by reCAPTCHA and the <br/> Google Privacy Policy and Terms of Service apply.
+     </h5>
+     <div className="socials">
+      <h1>Follow us</h1>
+      <img src={socialsImg} alt="social media" />
+     </div>
+     </div>
+
+     {/*  */}
+     <div className="left"></div>
    </div>
    </div>
    </> 
