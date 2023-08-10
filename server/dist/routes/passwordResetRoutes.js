f4f6da1bb2b7919dfdd108f8cd5edd3620ab80b8
manan-eth <abdul.suleman@amalitech.org>
2023-08-10 12:22:38 +0000

    Hotfixes

--- server/dist/routes/passwordResetRoutes.js
+++ server/dist/routes/passwordResetRoutes.js
@@ -7,5 +7,5 @@ const express_1 = __importDefault(require("express"));
 const passwordReset_1 = require("../controllers/passwordReset");
 const router = express_1.default.Router();
 router.post("/check-email", passwordReset_1.resetVerifyEmail);
-router.post("reset-password/:id/:token", passwordReset_1.changePassword);
+router.post("/reset-password/:id/:token", passwordReset_1.changePassword);
 exports.default = router;
