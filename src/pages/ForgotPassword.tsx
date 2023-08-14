b28cc779362c0d83c79c7a126445ce8a531d119d
Aboagye=Heming <hemingkwabena@gmail.com>
2023-08-14 04:18:21 +0000

    fixes on the login pages

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -31,7 +31,7 @@ const ForgotPassword: React.FC = () => {
       <div className="form">
         <div className="form__inner">
           <img className="form__logo" src={mainLogo} alt="logo" />
-          <h2 className="form__title reset">Forgot Password</h2>
+          <h2 className="form__title reset">Forgot Password?</h2>
           <p className="terms ">
             No worries, we will send you reset instructions
           </p>
