9929eb65edc46f809037edeae5618804525509d7
Felix Ekow Amoquandoh <felix.amoquandoh@amalitech.org>
2023-08-15 06:20:43 +0000

    added new component

---
+++ src/components/TextHeading/TextHeading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./textHeading.css"
+
+
+
+const HeadingWithText: React.FC = () => {
+  return (
+    <>
+    <div className=''>
+     tttttt
+    </div>
+    </>
+  );
+};
+
+export default HeadingWithText;
+
+  
+export {}; 
